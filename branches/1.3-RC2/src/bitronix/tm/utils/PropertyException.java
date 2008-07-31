32be2eba11b1b2e3f366a10984ba469e0d7d598c
lorban <lorban@db562501-c12d-0410-ab75-b5d49c0ca9de>
2008-07-31 12:59:31 +0000

    1.3-RC2 branch

---
+++ branches/1.3-RC2/src/bitronix/tm/utils/PropertyException.java
@@ -0,0 +1,19 @@
+package bitronix.tm.utils;
+
+/**
+ * Thrown by {@link PropertyUtils} when some reflection error occurs.
+ * <p>&copy; Bitronix 2005, 2006, 2007</p>
+ *
+ * @author lorban
+ */
+public class PropertyException extends RuntimeException {
+
+    public PropertyException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PropertyException(String message) {
+        super(message);
+    }
+
+}
